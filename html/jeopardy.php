7e44c272a9231c8a53e3bc52325237ebb2024f6a
dcoffey3296 <danielpcoffey@gmail.com>
2013-11-16 00:12:30 -0500

    more progress

--- html/jeopardy.php
+++ html/jeopardy.php
@@ -22,9 +22,10 @@ if ($game = json_decode(file_get_contents("boards/" . $_GET["f"])) == false)
 
 	<head>
 		<title>Jeopardy50</title>
-		<script src="//codeorigin.jquery.com/jquery-latest.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+		<script src="lib/FitText.js/jquery.fittext.js"></script>
 		<script src="js/jeopardy.js"></script>
 
 		<link rel="stylesheet" type="text/css" href="css/jeopardy.css"/>
@@ -34,14 +35,23 @@ if ($game = json_decode(file_get_contents("boards/" . $_GET["f"])) == false)
 		</script>
 	</head>
 	<body>
-		<div id="score">
-			<div><?= $_GET["t1"] ?></div>
-			<div><?= $_GET["t2"] ?></div>
+		<div id="score_holder">
+			<div id="t1_holder" class="scorecard"><?= $_GET["t1"] ?><div id="t1_score">0</div></div>
+			<div id="t2_holder" class="scorecard"><?= $_GET["t2"] ?><div id="t2_score">0</div></div>
 		</div>
 		
 
-		<div id="board">
-
+		<div id="board_holder">
+			<div id="fullscreen" style="display:none" class="square">
+				<div id="question"></div>
+				<div id="award_holder">
+					<table id="award">
+						<tr>
+							<td><?= $_GET["t1"] ?></td><td id="cancel">cancel</td><td><?= $_GET["t2"] ?></td>
+						</tr>
+					</table>
+				</div>
+			</div>
 		</div>
 	</body>
 
--- html/js/jeopardy.js
+++ html/js/jeopardy.js
@@ -5,16 +5,28 @@ $(document).ready(function(){
 	load_board();
 
 	// set up event listeners
-	$("td").bind("click", event, function(){
-		get_question(event);
+	$("#board td").bind("click", event, function(){
+		show_question(event);
+	});
+
+	$("#cancel").bind("click", event, function(){
+		close_fullscreen();
 	});
 });
 
-function get_question(click){
+function close_fullscreen(){
+	$("#fullscreen").fadeOut(function(){
+		$("#question").html("");
+	});
+}
+
+function show_question(click){
 	var element = click.target;
 	column = $(element).attr('id').replace(/^\D+/g, '');
 	row = $(element).parent().attr('id').replace(/^\D+/g, '');
 
+	$("#question").html(window.board[row][column].question.q).fitText(1.0, { minFontSize: '100px'});
+	$("#fullscreen").fadeIn();
 	console.log(window.board[row][column].question.q + " for " + window.board[row][column].value);
 }
 
@@ -60,27 +72,40 @@ function load_board(){
 
 function draw_board()
 {
-	var table = "<table>";
+	var table = "<table id='board'>";
 	for (var i = 0; i <= window.board.height; i++)
 	{
-		table += "<tr id='r-" + (i - 1) + "'>";
+		if (i == 0)
+		{
+			table += "<tr id='first'>";
+		}
+		else
+		{
+			table += "<tr id='r-" + (i - 1) + "'>";
+		}
+		
 		for (var j = 0; j <= window.board.width - 1; j++)
 		{
 			if (i == 0)
 			{
-				table += "<th>" + window.categories[j] + "</th>";
+				table += "<th style='height: " + (100 / (window.board.height + 1)) + "%; width: " + (100 / window.board.width) + "%' class='square'>" + window.categories[j] + "</th>";
 			}
 			else
 			{
-				table += "<td id='c-" + j + "'>" + window.board[i - 1][j].value + "</td>";
+				table += "<td id='c-" + j + "' style='height: " + (100 / (window.board.height + 1)) + "%; width: " + (100 / window.board.width) + "%' class='square'>" + window.board[i - 1][j].value + "</td>";
 			}
 		}
 		table += "</tr>";
 	}
 	table += "</table>"
 
-	$("#board").append(table);
-	console.log(table);
+	$("#board_holder").append(table);
+	jQuery("th").fitText(0.85);
+	jQuery("td").fitText(0.45);
+	jQuery("#award td").fitText(1.0, { minFontSize: '50px'});
+	jQuery("#t1_holder").fitText(1.0, { minFontSize: '30px'});
+	jQuery("#t2_holder").fitText(1.0, { minFontSize: '30px'});
+
 }
 
 
